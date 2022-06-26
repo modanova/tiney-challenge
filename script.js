ee2b5c75c7553443d20943bf6923a673a6f80baa
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-06-26 21:31:38 +0100

    Test js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function signIn(this) {
-    let signIn = this;
-    console.log(this);
-}
+// let modal = document.getElementsByClassName("modal")[0];
+// let btn = document.getElementById("submit");
+// let span = document.getElementsByClassName("close");
+
+// btn.onclick = function() {
+//     modal.classList.toggle("hidden");
+//   }
+
+// span.onclick = function() {
+//     modal.classList.toggle("hidden");
+//   }
+
+// window.onclick = function(event) {
+//     if (event.target == signIn) {
+//         modal.classList.toggle("hidden");
+//     }
+//   }
+
+// console.log('it works!');
