b5cac1e2aff6591f089bc3ff97c3784492e4eb40
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-06-28 21:45:34 +0100

    CreatePupilElement works

--- script.js
+++ script.js
@@ -17,3 +17,73 @@
 //   }
 
 // console.log('it works!');
+const register = document.getElementById("register");
+const totalCounter = document.getElementById("total");
+
+const createPupilElement = (pupil) => {
+    // Create a div from the template with class 'template-pupil'
+    const template = document.querySelector('.template-pupil');
+    const newPupil = template.content.cloneNode(true);
+    newPupil.querySelector(".img-pupil").src = pupil.avatar;
+    return newPupil;
+}
+
+const signIn = (event) => {
+    // TODO: Open the confirm sign-in modal and populate all the data
+}
+
+const confirmSignIn = (event) => {
+    // TODO: Mark the student as signed in and update its element
+    // TODO: hide signIn and show signOut
+    // TODO: hide the modal
+    // savePupils(...)
+}
+
+const reportAbsent = (event) => {
+    // TODO: Mark the student as absent and update its element
+    // TODO: hide signIn and show absent
+    // TODO: hide the modal
+    // savePupils(...)
+}
+
+const signOut = (event) => {
+    // TODO: Mark the student as signed out and update its element
+    // TODO: hide signOut and show signIn or whatever needs to be shown
+    // savePupils(...)
+}
+
+const savePupils = (pupils) => {
+    window.localStorage.setItem("pupils", JSON.stringify(pupils));
+}
+
+const loadPupils = async () => {
+    let pupils = null;
+    try {
+        pupils = JSON.parse(window.localStorage.getItem("pupils"));
+        if (!pupils) throw Error("No pupils in local storage");
+    } catch {
+        console.log("Cannot load pupils, fetch from data/pupils.json");
+        pupils = await fetch("data/pupils.json").then((x)=>x.json());
+    }
+    return pupils;
+}
+
+const main = async () => {
+    // register.replaceChildren(); // remove all default elements, or remove them from the html manually
+    // 1. Load all pupils
+    const pupils = await loadPupils();
+    // 2. Loop through all pupils and create their elements
+    const elements = pupils.map(createPupilElement);
+    // TODO: 3. Append those elements
+    elements.forEach((el) => {
+        // const hr = document.createElement("hr");
+        // register.appendChild(hr);
+        // register.appendChild(el);
+    });
+    // TODO: 4. Add event listeners to all Sign-in/Sign out buttons
+    // const signIns = document.getElementsByClassName("sign-in");
+    // const signOuts = document.getElementsByClassName("sign-out");
+    // ... forEach(x => x.addEventListener('click', signIn/singOut))
+}
+
+main();
